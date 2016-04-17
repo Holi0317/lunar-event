0e02d06b4775c5c1d35d08ab3c8c34f6c9f14515
Holi0317 <holliswuhollis@gmail.com>
2016-04-17 12:31:18 +0800

    [FIX] Incorrect CSS props in LoginedView

--- app/components/LoginedView.jsx
+++ app/components/LoginedView.jsx
@@ -8,7 +8,7 @@ export default function LoginedView() {
   return (
     <Paper style={{padding: '10px'}}>
       <h1>新增事件</h1>
-      <small style={{display: 'block', 'margin-bottom': '5px'}}>在這裏新增一個農曆重複事件, 就會被加入到Google日曆上</small>
+      <small style={{display: 'block', marginBottom: '5px'}}>在這裏新增一個農曆重複事件, 就會被加入到Google日曆上</small>
       <Divider />
 
       <EventForm />
